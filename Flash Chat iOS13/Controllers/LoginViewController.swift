469a2579cf18f4815eaae42f6861f89e9f6bb785
Bokhulenkov <bokhulenkov@gmail.com>
2024-07-18 10:00:20 +0300

    create login user

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -7,14 +7,26 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
-
+    
     @IBAction func loginPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+//                убираем сильную ссылку и проверяем наличие self в памяти
+                guard let strongSelf = self else { return }
+                if  let error = error {
+                    self?.showAlert(message: error.localizedDescription)
+                } else {
+                    self?.performSegue(withIdentifier: "LoginToChat", sender: strongSelf)
+                }
+            }
+        }
     }
     
 }
